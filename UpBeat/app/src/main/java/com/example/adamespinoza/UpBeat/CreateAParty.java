ac6d17ff5fa8ceba8db3951630a70c958719be46
Devil's Door <beideng@The-Impala-2.local>
2015-10-11 11:34:02 -0500

    text edit

--- UpBeat/app/src/main/java/com/example/adamespinoza/UpBeat/CreateAParty.java
+++ UpBeat/app/src/main/java/com/example/adamespinoza/UpBeat/CreateAParty.java
@@ -13,9 +13,7 @@ public class CreateAParty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createparty);
-<<<<<<< HEAD
 
-=======
         /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 >>>>>>> origin/master
