ab08f2d82f65eb1767c76b112a1a3c78df0a1cfd
Devil's Door <beideng@The-Impala-2.local>
2015-10-11 11:21:38 -0500

    main changes

--- UpBeat/app/src/main/java/com/example/adamespinoza/UpBeat/CreateAParty.java
+++ UpBeat/app/src/main/java/com/example/adamespinoza/UpBeat/CreateAParty.java
@@ -12,18 +12,20 @@ public class CreateAParty extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_aparty);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_createparty);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        // fab.setOnClickListener(new View.OnClickListener() {
+        //@Override
+        //public void onClick(View view) {
+        //  Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //        .setAction("Action", null).show();
+        //  }
+        // });
+        //}
+
     }
-
 }
+
